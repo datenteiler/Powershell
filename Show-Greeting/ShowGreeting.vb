074b116a205e378878bf50878503094929b48c7d
Christian Imhorst <christian.imhorst@gmail.com>
2015-05-13 09:05:52 +0200

    Create ShowGreeting.vb

---
+++ Show-Greeting/ShowGreeting.vb
@@ -0,0 +1,32 @@
+Imports System.Management.Automation ' Windows PowerShell assembly
+
+Namespace ShowGreeting
+    ' Declare the class as a cmdlet and specify and 
+    ' appropriate verb and noun for the cmdlet name.
+    <Cmdlet(VerbsCommon.Show, "Greeting")> _
+    Public Class SendGreetingCommand
+        Inherits Cmdlet
+
+        ' Declare the parameter for the cmdlet
+        Private _Name As String()
+        <Parameter(Position:=0, Mandatory:=True), ValidateNotNullOrEmpty()> _
+        Public Property Name() As String() ' Sets name of parameter
+            Get
+                Return _Name
+            End Get
+            Set(ByVal value As String())
+                _Name = value
+            End Set
+        End Property
+
+        ' Overide the ProcessRecord method to process
+        ' the supplied user name and write out a 
+        ' greeting to the user by calling the WriteObject
+        'method.
+        Protected Overrides Sub ProcessRecord()
+            ' Convert _Name to string 
+            Dim strName As String = String.Join(" ", _Name)
+            WriteObject("Hallo " + strName + "!")
+        End Sub
+    End Class
+End Namespace
