c85a5460fde797713d5978a56eae9c12d2c84905
Christian Imhorst <christian.imhorst@gmail.com>
2021-04-05 16:27:30 +0200

    Create addiere.c

---
+++ PInvoke-Examples/addiere/addiere.c
@@ -0,0 +1,13 @@
+#include "addiere.h"
+
+int addiere(int augend, int addend) {
+	return (augend + addend);
+}
+
+/*
+int main() {
+	int summe = addiere(23, 77);
+	printf("23 + 77 = %d\n", summe);
+	return 0;
+}
+*/
